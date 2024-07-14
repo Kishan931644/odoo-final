6dbc1ea534a5be007cee03d6308307388d1924af
Jainil <jainilprajapati8095@gmail.com>
2024-07-14 16:06:02 +0530

    chore: Remove unnecessary route in user.js

--- frontend/src/assets/Components/Index.jsx
+++ frontend/src/assets/Components/Index.jsx
@@ -1,10 +1,38 @@
 import Header from "./Header.jsx";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import "../CSS/Home.css";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 export default function Index() {
     const [book, setBook] = useState([]);
+    const [popularBooks, setPopularBooks] = useState([]);
+    const [recommendedBooks, setRecommendedBooks] = useState([]);
+
+    const [user, setProfile] = useState(null);
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            let headersList = {
+                "Accept": "*/*",
+                "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+                "Authorization": localStorage.getItem("token")
+            }
+
+            let response = await fetch("http://localhost:3000/api/user/getowndetails", {
+                method: "POST",
+                headers: headersList
+            });
+
+            let data = await response.json();
+            if (data.status == "success") {
+                let user = data.data.user;
+                setProfile(user);
+            }
+        }
+
+        fetchUser();
+    }, []);
+
 
     useEffect(() => {
         const fetchBooks = async () => {
@@ -21,25 +49,69 @@ export default function Index() {
             let data = await response.json();
             if (data.status == "success") {
                 let listOfBooks = data.data.books;
-                setBook(listOfBooks);
+                setPopularBooks(listOfBooks);
             }
         }
+
+        const fetchRecommendedBooks = async () => {
+            let headersList = {
+                "Accept": "*/*",
+                "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+                "Authorization": localStorage.getItem("token")
+            }
+
+            let response = await fetch("http://localhost:3000/api/book/getrecommodadedbooks", {
+
+                method: "GET",
+                headers: headersList
+            });
+
+            let data = await response.json();
+            if (data.status == "success") {
+                let listOfBooks = data.data.books;
+                setRecommendedBooks(listOfBooks);
+            }
+        }
+
+        const fetchPopularBooks = async () => {
+            let headersList = {
+                "Accept": "*/*",
+                "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+                "Authorization": localStorage.getItem("token")
+            }
+
+            let response = await fetch("http://localhost:3000/api/book/getpopularbooks", {
+                method: "GET",
+                headers: headersList
+            });
+
+            let data = await response.json();
+            if (data.status == "success") {
+                let listOfBooks = data.data.popularBooks;
+                setPopularBooks(listOfBooks);
+            }
+        }
+
         fetchBooks();
+        fetchRecommendedBooks();
+        fetchPopularBooks();
     }, []);
     return (
         <>
             <Header childrens={
                 <button type="button">
-                    {(localStorage.getItem("token")==null) ?( <Link to="/login">Login</Link>) : (
+                    {(localStorage.getItem("token") == null) ? (<Link to="/login">Login</Link>) : (
                         <Link to="/logout">Log out</Link>)}
-                </button>}/>
+                </button>}
+
+            />
             <main className="main-container">
                 <div className="home-header">
                     <h2>Search the book available in Library</h2>
                     <form role="search">
                         <div className="input-container">
                             <input type="search" className="search-input" value="" name="s"
-                                   placeholder="Search Book name,author"/>
+                                placeholder="Search Book name,author" />
                             <button type="submit" className="search-submit" name="submit"><i
                                 className="material-icons">search</i></button>
                         </div>
@@ -53,7 +125,7 @@ export default function Index() {
 
                             {book && <div className="book-card">
                                 <div className="book-img-container">
-                                    <img src="/Img/sample%20book.jpg" alt=""/>
+                                    <img src="/Img/sample%20book.jpg" alt="" />
                                 </div>
                                 <div className="content">
                                     <Link to={"/"} className="book-name">{book.title}</Link>
@@ -66,58 +138,55 @@ export default function Index() {
                             </div>}
                         </div>
                     </div>
+                    {
+                        user == null && (<div className="trending-bok">
+                            <h2>Trending Available</h2>
+                            <div className="books-container">
+                                {
+                                    popularBooks && popularBooks.map((book) => (
 
-                    <div className="trending-bok">
-                        <h2>Trending Available</h2>
-                        <div className="books-container">
-                            <div className="book-card">
-                                <div className="book-img-container">
-                                    <img src="/Img/sample%20book.jpg" alt=""/>
+                                        <div className="book-card">
+                                            <div className="book-img-container">
+                                                <img src={book.thumnailLink} alt="" />
+                                            </div>
+                                            <div className="content">
+                                                <Link to={"/"} className="book-name">{book.title}</Link>
+                                                <div className="details">
+                                                    <div className="author">{book.auther}</div>
+                                                    <div className="realese-year">{book.year}</div>
+                                                </div>
+                                                <div className="description">{book.desc}</div>
+                                            </div>
+                                        </div>
+
+                                    ))
+                                }
+
+                            </div>
+                        </div>)
+                    }
+                    {
+                        user != null && (<div className="profile-container">
+
+                            <div className="profile">
+                                <div className="profile-img">
+                                    <img src="/Img/Profile%20Image.png" alt="" />
                                 </div>
-                                <div className="content">
-                                    <Link to={"/"} className="book-name">Python Tricks</Link>
-                                    <div className="details">
-                                        <div className="author">Parth gajjar,Ayushi khana</div>
-                                        <div className="realese-year"> . 2020</div>
-                                    </div>
-                                    <div className="description">Lorem Lorem ipsum dolor sit amet, consectetur
-                                        adipisicing elit. Consequatur distinctio dolore odio veniam. A commodi culpa
-                                        cupiditate eaque eius ipsa itaque iusto laboriosam magnam maiores, minima modi
-                                        nam officia quaerat quas qui quidem sapiente suscipit tempore ullam vel
-                                        veritatis vitae voluptatibus! Amet at autem commodi cumque doloremque enim eos,
-                                        hic iusto laborum, minus mollitia obcaecati possimus praesentium quia
-                                        voluptatum. Animi, aperiam aspernatur assumenda dicta distinctio facere fuga
-                                        ipsa ipsum libero nemo nisi nobis, nostrum quos reprehenderit voluptatibus. Ad
-                                        animi ea eius illo iste minus perferendis, quas ratione reiciendis voluptatem!
-                                        Assumenda atque deserunt dolorem eligendi facilis impedit in incidunt ipsa iure
-                                        maxime modi natus, nesciunt odio qui quisquam quod sint sit tempore ut
-                                        voluptate. Ad adipisci culpa cupiditate distinctio dolores ducimus, eligendi,
-                                        fugiat impedit incidunt, ipsam itaque maiores minima molestiae nam perspiciatis
-                                        quidem quo quod reprehenderit sunt temporibus velit voluptates voluptatum. Ab
-                                        accusantium ad alias amet architecto at consectetur consequatur delectus eos et
-                                        eveniet expedita explicabo fugiat hic illum incidunt itaque iure laboriosam
-                                        laborum laudantium magnam maiores nihil nostrum numquam obcaecati odio odit
-                                        officia omnis perspiciatis rem, repudiandae saepe soluta tempora temporibus unde
-                                        vel voluptate? Aspernatur consectetur hic ipsum perspiciatis suscipit! Ab
-                                        asperiores assumenda at eius inventore iusto libero nesciunt non odit, omnis
-                                        perspiciatis possimus quo voluptate, voluptatem voluptatum! Adipisci explicabo
-                                        minima nobis quibusdam reiciendis! At, commodi optio? Alias consequuntur enim
-                                        facere iusto, maiores minus necessitatibus officia similique totam voluptate. A
-                                        aperiam assumenda minus nobis officia! Ab amet aperiam culpa cum cupiditate,
-                                        esse est illo odio porro provident quas quidem quos repudiandae totam unde vero
-                                        voluptatem! Aliquid, architecto asperiores, cumque esse eum ex excepturi iste
-                                        modi officia repellat sint tempore ullam veritatis! Ex, mollitia omnis quaerat
-                                        qui unde ut. Alias cum eaque eligendi enim excepturi, explicabo fugiat in modi
-                                        natus non officiis, quam repellendus? Error expedita minima quam repellendus,
-                                        tempore veritatis! ipsum dolor sit amet, consectetur adipisicing elit.
-                                        Aspernatur, delectus, dolorum est exercitationem labore laboriosam natus,
-                                        obcaecati perspiciatis placeat quam quo saepe ut vitae. Aperiam eos facilis fuga
-                                        molestias nihil.
-                                    </div>
+                                <div className="profile-details">
+                                    <div className="profile-role">Role: {user.role}</div>
+                                    <div className="profile-name">Name: {user.name}</div>
+                                    <div className="profile-">Email: {user.email}</div>
+                                    <div className="profile-address">City: {user.city}</div>
+                                    <div className="profile-country">Country: {user.country}</div>
                                 </div>
+
                             </div>
                         </div>
-                    </div>
+                        )
+
+                    }
+
+
                 </div>
             </main>
         </>
